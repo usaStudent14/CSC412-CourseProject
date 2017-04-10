e4fc07d90ac47bc383b92c5c434d9c745508a40b
Matt D <vonspot@gmail.com>
2017-04-10 16:29:36 -0500

    testing 3 lights

--- CourseProject/Brain.h
+++ CourseProject/Brain.h
@@ -7,25 +7,22 @@
 
 class Brain
 {
-    public:
-        static const int MIN_LEFT_LIGHT  = 350;
-        static const int MAX_LEFT_LIGHT  = 530;
-        static const int MIN_RIGHT_LIGHT = 400;
-        static const int MAX_RIGHT_LIGHT = 560;
-        static const int MIN_TURN_DELTA  = -60;
-        static const int MAX_TURN_DELTA  = 60;
-        
-        NXTLight *leftLight;
-        NXTLight *rightLight;
-        NXTUS    *sonar;
-
-        int   sonarReal      = 0;
-        int   turnDeltaReal  = 0;
-        float leftLightReal  = 0.0;
-        float rightLightReal = 0.0;
-
-        void think();
-        int clamp(int lower, int value, int upper);
+  public:
+    static const int THRESHOLD = 475;
+    
+    NXTLight *m_leftLight;
+    NXTLight *m_centerLight;
+    NXTLight *m_rightLight;
+    NXTUS    *m_sonar;
+  
+    bool leftFlag   = false;
+    bool centerFlag = true;
+    bool rightFlag  = false;
+    
+    int targetHeading = 0;
+    
+    void think();
+    static int clamp(int lower, int value, int upper);
 };
 
 #endif
